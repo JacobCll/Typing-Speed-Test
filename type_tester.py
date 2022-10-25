22674d1328e7e617fba271ec635f51f9035586b7
JacobCll <108395026+JacobCll@users.noreply.github.com>
2022-10-25 14:28:02 +0800

    Update type_tester.py

--- type_tester.py
+++ type_tester.py
@@ -14,15 +14,14 @@ words_length = len(words)
 print("Type the given words:")
 print(words)
 
-t1 = time.time()
+t1 = time.time() # start time
 typed = input() # times user input
-t2 = time.time()
-time_elapsed = t2 - t1 
+t2 = time.time() # end time
+time_elapsed = (t2 - t1)
 print(round(time_elapsed), 'seconds')
 minutes = (time_elapsed / 60)
 
 keys_pressed = len(typed)
-# print("Total keys pressed:", keys_pressed)
 correct_keys_pressed = 0
 
 for l in range(len(words)):
@@ -31,9 +30,7 @@ for l in range(len(words)):
             correct_keys_pressed += 1
     except: 
         correct_keys_pressed += 0
-
-# print("Correct keys pressed:", correct_keys_pressed)
-
+        
 wpm = (correct_keys_pressed / 5) / minutes
 accuracy = (correct_keys_pressed / len(words)) * 100
 awpm = (accuracy / 100) * wpm #adjusted wpm
